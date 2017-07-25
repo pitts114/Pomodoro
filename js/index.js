2763dcd1cff418614c02eca4ea1fc1e67deb21dd
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-07-24 21:07:19 -0400

    Circle Clock

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+
+$(document).ready(function() {
+    var workTimer = document.createElement("p");
+    var node = document.createTextNode("10:00");
+    workTimer.appendChild(node);
+    var timerCircle = document.getElementById("circle");
+    timerCircle.appendChild(workTimer);
+});
+*/
